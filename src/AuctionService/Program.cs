14a763789f500c9fcb701258de44e77b487c2f9e
Henrique Cunha <henrique.cunha@hexagon.com>
2023-09-28 23:47:01 -0300

    chore: create AuctionService with Initial database migration

---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,20 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
