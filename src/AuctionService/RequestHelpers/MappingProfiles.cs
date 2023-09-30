933ceb526285dd433a0e84ac3fd81690caf614f3
Henrique Cunha <henrique.cunha@hexagon.com>
2023-09-30 13:41:01 -0300

    feat: add AutoMapper and configure Mapper transformations on MappingProfiles

---
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,17 @@
+﻿using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace AuctionService.RequestHelpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+        CreateMap<Item, AuctionDto>();
+        CreateMap<CreateAuctionDto, Auction>()
+            .ForMember(destination => destination.Item, options => options.MapFrom(s => s));
+        CreateMap<CreateAuctionDto, Item>();
+    }
+}
