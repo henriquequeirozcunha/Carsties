cc3cd52f789a857034d7ce5a83584a0d8a8eccb4
Henrique Cunha <henrique.cunha@hexagon.com>
2023-10-01 14:17:32 -0300

    feat: add Auctions controller

--- src/AuctionService/DTOs/UpdateAuctionDto.cs
+++ src/AuctionService/DTOs/UpdateAuctionDto.cs
@@ -4,14 +4,9 @@ namespace AuctionService.DTOs;
 
 public class UpdateAuctionDto
 {
-    [Required]
     public string Make { get; set; }
-    [Required]
     public string Model { get; set; }
-    [Required]
-    public int Year { get; set; }
-    [Required]
+    public int? Year { get; set; }
     public string Color { get; set; }
-    [Required]
-    public int Mileage { get; set; }
+    public int? Mileage { get; set; }
 }
