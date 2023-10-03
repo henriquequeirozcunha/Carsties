b7d72525a730255f5e59b1e031934c008e177117
Henrique Cunha <henrique.cunha@hexagon.com>
2023-10-03 19:31:20 -0300

    refactor: add retry strategy to AuctionCreatedConsumer

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -20,6 +20,12 @@ builder.Services.AddMassTransit(x =>
 
     x.UsingRabbitMq((context, cfg) =>
     {
+        cfg.ReceiveEndpoint("search-auction-created", e =>
+        {
+            e.UseMessageRetry(r => r.Interval(5, 5));
+            e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+        });
+
         cfg.ConfigureEndpoints(context);
     });
 });
