e829eaef48ae6f5b83a92f31cbdcd8a039fa3dd7
Henrique Cunha <henrique.cunha@hexagon.com>
2023-10-03 20:32:36 -0300

    feat: add AuctionCreatedFaultyConsumer in case SearchService(MongoDB) fails

---
+++ src/AuctionService/Consumers/AuctionCreatedFaultConsumer.cs
@@ -0,0 +1,24 @@
+﻿using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers;
+
+public class AuctionCreatedFaultConsumer : IConsumer<Fault<AuctionCreated>>
+{
+    public async Task Consume(ConsumeContext<Fault<AuctionCreated>> context)
+    {
+        Console.WriteLine("-----> Consuming fault creation");
+
+        var exception = context.Message.Exceptions.First();
+
+        if (exception.ExceptionType == "System.ArgumentException")
+        {
+            context.Message.Message.Model = "NOT_ERRORMODEL"; // NOTE: THIS IS AN EXAMPLE OF CHANGE THE MESSAGE FROM THE FAULTY QUEUE.
+            await context.Publish(context.Message.Message);
+        }
+        else
+        {
+            Console.WriteLine("Not an argumento exception - update error dashboard");
+        }
+    }
+}
