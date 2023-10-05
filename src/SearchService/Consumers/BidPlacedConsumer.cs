fce7cb29b1caeeceb4a49c764eae2641f94a6783
Henrique Cunha <henrique.cunha@hexagon.com>
2023-10-05 19:44:49 -0300

    feat: add consumers to SearchService

---
+++ src/SearchService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,25 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("---> Consuming bid placed");
+
+        var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+        var bidValidForHighBid = context.Message.BidStatus.Contains("Accepted")
+            && context.Message.Amount > auction.CurrentHighBid;
+
+        if (bidValidForHighBid)
+        {
+            auction.CurrentHighBid = context.Message.Amount;
+            await auction.SaveAsync();
+        }
+    }
+}
