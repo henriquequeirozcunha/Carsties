b43b9642cc0df06c582fb98da006970f52de9c30
Henrique Cunha <henrique.cunha@hexagon.com>
2023-10-20 00:28:18 -0300

    feat: add fetchWrapper

--- frontend/web-app/app/services/auction.ts
+++ frontend/web-app/app/services/auction.ts
@@ -4,6 +4,7 @@ import { Auction } from '@/models/auction'
 import { PaginatedResult } from '@/models/paginatedResult'
 import qs from 'query-string'
 import { getTokenWorkaround } from '../auctions/authActions'
+import { fetchWrapper } from '@/lib/fetchWrapper'
 
 type getAuctionsDto = {
   pageNumber: number
@@ -15,15 +16,7 @@ export async function getAuctions(
 ): Promise<PaginatedResult<Auction>> {
   const queryParams = qs.stringifyUrl({ url: '', query: params })
 
-  // const res = await fetch(
-  //   `http://localhost:6001/search?pageSize=${params.pageSize}&pageNumber=${params.pageNumber}`
-  // )
-
-  const res = await fetch(`http://localhost:6001/search${queryParams}`)
-
-  if (!res.ok) throw new Error('Failed to fetch data')
-
-  return res.json()
+  return await fetchWrapper.get(`search${queryParams}`)
 }
 
 export async function updateAuctionTest() {
