3b4d8a29e5e50b5490d6df9f98520e5bed1e596a
Henrique Cunha <henrique.cunha@hexagon.com>
2023-11-28 00:39:11 -0300

    fix: some fixes

--- frontend/web-app/app/lib/fetchWrapper.ts
+++ frontend/web-app/app/lib/fetchWrapper.ts
@@ -76,7 +76,10 @@ async function handleResponse(response: Response) {
   } else {
     const error = {
       status: response.status,
-      message: typeof data === 'string' ? data : response.statusText,
+      message:
+        typeof data === 'string' && data.length > 0
+          ? data
+          : response.statusText,
       errors: data.errors,
       title: data.title,
     }
